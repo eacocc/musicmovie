f310331276bd9d1be87844cc5314f94017f1991c
eacocc <eaco2cc@gmail.com>
2025-08-20 10:05:53 +0800

    Create config.js

---
+++ v011/config.js
@@ -0,0 +1,12 @@
+const EACO_CONFIG = {
+  tmdbKey: 'YOUR_TMDB_KEY',
+  omdbKey: 'YOUR_OMDB_KEY',
+  traktKey: 'YOUR_TRAKT_KEY',
+  simklKey: 'YOUR_SIMKL_KEY',
+  unogsKey: 'YOUR_UNOGS_KEY',
+  watchmodeKey: 'YOUR_WATCHMODE_KEY',
+  spotifyToken: 'YOUR_SPOTIFY_BEARER_TOKEN',
+  soundcloudKey: 'YOUR_SOUNDCLOUD_KEY',
+  lastfmKey: 'YOUR_LASTFM_KEY',
+  apioopenKey: 'YOUR_APIOPEN_KEY'
+};
